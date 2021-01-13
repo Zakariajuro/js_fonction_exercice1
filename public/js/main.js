31e8a9809dfd73802e29f67b4bd028f799da4ecf
Zakaria <zakPro@live.be>
2021-01-13 11:05:31 +0100

    exo-fini-sauf-point9

---
+++ public/js/main.js
@@ -0,0 +1,107 @@
+//  EXERCICE 1
+
+// let baseNumb = (nomb1, nomb2) =>{
+//     return `${nomb1} + ${nomb2} = ${nomb1+nomb2}`
+// }
+
+// let result = baseNumb(10, 20);
+// console.log(result);
+
+
+
+// EXERCICE 2
+
+// let baseNumb = (nomb1, nomb2) =>{
+//     return `${nomb1} - ${nomb2} = ${nomb1-nomb2}`
+// }
+
+// let result = baseNumb(20, 10);
+// console.log(result);
+
+
+// EXERCICE 3
+
+// let baseNumb = (nomb1, nomb2) =>{
+//     return `${nomb1} x ${nomb2} = ${nomb1*nomb2}`
+// }
+
+// let result = baseNumb(20, 10);
+// console.log(result);
+
+
+// EXERCICE 4
+
+// let baseNumb = (nomb1, nomb2) =>{
+//     return `${nomb1} / ${nomb2} = ${nomb1/nomb2}`
+// }
+
+// let result = baseNumb(20, 4);
+// console.log(result);
+
+// EXERCICE 5
+
+// let nombre = (a, b) =>{
+//     return a%b
+// }
+// let result = nombre(20, 10);
+// console.log(result);
+
+
+// EXERCICE 6
+
+// let nombre = (a) =>{
+//     return Math.sqrt(a)
+// }
+
+// let result = nombre(9);
+// console.log(result);
+
+
+// EXERCICE 7
+
+// let nombre = (x) => {
+//     return x*x
+// }
+
+// let result = nombre(4);
+// console.log(result);
+
+
+// EXERCICE 8
+
+// let capitalize = (a) => {
+//     return a.charAt(0).toUpperCase() + a.slice(1)
+// }
+
+// let result = capitalize('zakaria');
+// console.log(result);
+
+
+// EXERCICE 9
+
+
+let calcul = (nbr1 , operator , nbr2) => {
+    return `${nbr1} ${operator} ${nbr2} = ${nbr1,operator,nbr2}`
+}
+// switch (calcul) {
+
+//     case "+":
+//         operator = "+";
+//         break;
+//     case "-":
+//         operator = "-";
+//         break;
+//     case "*":
+//     operator = "*";
+//         break;
+//     case "/":
+//         operator = "/";
+//     break;
+
+//     default:
+//         break;
+// }
+
+let result = calcul(10,"+",20);
+
+console.log(result);
